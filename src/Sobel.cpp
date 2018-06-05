c16a2ce1c2fbb23e0b7819c473b638e8e76c5179
kolszewska <k.olszewskaaa@gmail.com>
2018-06-05 23:13:54 +0200

    Fixed issue with gpu sobel filter

--- src/Sobel.cpp
+++ src/Sobel.cpp
@@ -20,7 +20,7 @@ std::vector<unsigned char> Sobel::getNewChannelValues(std::vector<unsigned char>
     for (int x = 1; x < image_width * image_height - 2; x++) {
         int x_gradient = computeXGradient(channel, x, image_width);
         int y_gradient = computeYGradient(channel, x, image_width);
-        new_channel_values.push_back(normalizeGradient(computeGradientLength(x_gradient, y_gradient)));
+        new_channel_values.push_back(normalizeGradient(computeGradientLength(x_gradient,  y_gradient)));
     }
     return new_channel_values;
 }
@@ -68,19 +68,15 @@ int Sobel::computeGradientLength(int gradient_x, int gradient_y) {
 int Sobel::normalizeGradient(int gradient_value) {
     if (gradient_value > 255) {
         return 255;
-    } else if (gradient_value < 0) {
-        return 0;
-    } else {
-        return gradient_value;
     }
+        return gradient_value;
 }
-
 void Sobel::applySobelFilterOnGpu(Image* image) {
-     clock_t begin = clock();
+    clock_t begin = clock();
 
-     unsigned char* out_r_channel;
-     unsigned char* out_g_channel;
-     unsigned char* out_b_channel;
+    unsigned char* out_r_channel;
+    unsigned char* out_g_channel;
+    unsigned char* out_b_channel;
 
     out_r_channel = cudaGetNewChannelValues(reinterpret_cast<unsigned char*> (image->r_channel.data()),
             image->width, image->height);
@@ -95,9 +91,13 @@ void Sobel::applySobelFilterOnGpu(Image* image) {
     double execution_time = double(end - begin) / CLOCKS_PER_SEC;
     printf("[GPU] Elapsed time: %.2f\n", execution_time);
 
+    out_r_channel = &out_r_channel[1];
+    out_g_channel = &out_g_channel[1];
+    out_b_channel = &out_b_channel[1];
+
     std::vector<unsigned char> r_channel(out_r_channel, out_r_channel + image->width * image->height);
-    std::vector<unsigned char> g_channel(out_r_channel, out_r_channel + image->width * image->height);
-    std::vector<unsigned char> b_channel(out_r_channel, out_r_channel + image->width * image->height);
+    std::vector<unsigned char> g_channel(out_g_channel, out_g_channel + image->width * image->height);
+    std::vector<unsigned char> b_channel(out_b_channel, out_b_channel + image->width * image->height);
 
     image->out_r_channel = r_channel;
     image->out_g_channel = g_channel;
@@ -105,4 +105,3 @@ void Sobel::applySobelFilterOnGpu(Image* image) {
 
     return;
 }
-
--- src/SobelFilterKernels.cu
+++ src/SobelFilterKernels.cu
@@ -1,56 +1,60 @@
-#include <stdio.h>
-#include <math.h>
 #include <cuda_runtime.h>
+#include <stdio.h>
 
 __global__ void cudaComputeXGradient(int* x_gradient, unsigned char* channel, int image_width, int image_height) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
-    if (index == 0) {
-        return;
-    }			
-    x_gradient[index] =
-        1 * channel[index - 1] +
-        2 * channel[index] +
-        1 * channel[index + 1] +
-        0 * channel[index + image_width - 1] +
-        0 * channel[index + image_width] +
-        0 * channel[index + image_width + 1] +
-        (-1) * channel[index + 2 * image_width - 1] +
-        (-2) * channel[index + 2 * image_width] +
-        (-1) * channel[index + 2 * image_width + 1];
+ if(index ==0) {
+	return;
+}
+int a[3][3] = { { 1, 0, -1 }, { 2, 0, -2 }, { 1, 0, -1 } }; 
+x_gradient[index] =
+        a[0][0] * channel[index - 1] +
+        a[1][0] * channel[index] +
+        a[2][0] * channel[index + 1] +
+        a[0][1] * channel[index + image_width - 1] +
+        a[1][1] * channel[index + image_width] +
+        a[2][1] * channel[index + image_width + 1] +
+        a[0][2] * channel[index + 2 * image_width - 1] +
+        a[1][2] * channel[index + 2 * image_width] +
+        a[2][2] * channel[index + 2 * image_width + 1];
     return;
 }
 
 __global__ void cudaComputeYGradient(int* y_gradient, unsigned char* channel, int image_width, int image_height) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
-    if (index == 0) {
-        return; 
-    }			
-    y_gradient[index] = 
-        1 * channel[index - 1] +
-        0 * channel[index] +
-        (-1) * channel[index + 1] +
-        2 * channel[index + image_width - 1] +
-        0 * channel[index + image_width] +
-        (-2) * channel[index + image_width + 1] +
-        1 * channel[index + 2 * image_width - 1] +
-        0 * channel[index + 2 * image_width] +
-        (-1) * channel[index + 2 * image_width + 1];
+    if(index ==0) {
+	return;
+}
+int b[3][3] = { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } }; 
+y_gradient[index] =
+        b[0][0] * channel[index - 1] +
+        b[1][0] * channel[index] +
+        b[2][0] * channel[index + 1] +
+        b[0][1] * channel[index + image_width - 1] +
+        b[1][1] * channel[index + image_width] +
+        b[2][1] * channel[index + image_width + 1] +
+        b[0][2] * channel[index + 2 * image_width - 1] +
+        b[1][2] * channel[index + 2 * image_width] +
+        b[2][2] * channel[index + 2 * image_width + 1];
     return;
 }
 
 __global__ void cudaComputeGradientLength(unsigned char *channel_values, int* x_gradient, int* y_gradient) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
-    channel_values[index] = sqrt(float(x_gradient[index] * x_gradient[index] + y_gradient[index] * y_gradient[index]));
+    int dupa = int(sqrt(float(x_gradient[index] * x_gradient[index] + y_gradient[index] * y_gradient[index])));
+    if (dupa > 255) {
+	dupa = 255;
+    }
+	channel_values[index] = dupa;
     return;
 }
 
 __global__ void cudaNormalizeGradient(unsigned char* channel_values) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     if (channel_values[index] > 255) {
-        channel_values[index] = 255;
-    } else if (channel_values[index] < 0) {
-        channel_values[index] = 0;
-    }
+        channel_values[index] = int(255);
+    } 
+    channel_values[index] = int(channel_values[index]);
     return;
 }
 
@@ -78,7 +82,7 @@ extern "C" unsigned char* cudaGetNewChannelValues(unsigned char* channel, int im
 
     cudaComputeGradientLength<<<blocks_per_grid, threads_per_block>>>(device_new_channel_values, x_gradient, y_gradient);
 
-    cudaNormalizeGradient<<<blocks_per_grid, threads_per_block>>>(device_new_channel_values);
+//    cudaNormalizeGradient<<<blocks_peir_grid, threads_per_block>>>(device_new_channel_values);
 
     unsigned char* host_new_channel_values = new unsigned char[image_width * image_height * sizeof(unsigned char)];
 	
